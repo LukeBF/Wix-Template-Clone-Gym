59d769ba517422b7637dab9494700fd27e5b439f
Luke Blache-Fraser <resolvetechnologiestt@gmail.com>
2021-08-12 00:25:14 -0400

    Working hamburger menu

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const showNav = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('show-nav')
+    })
+}
+
+showNav();
